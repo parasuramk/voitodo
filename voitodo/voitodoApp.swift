21c35acdc8375a155617a8ccb3c1c4d97de2b439
Parasuram Krishnan <kparasuram@gmail.com>
2026-03-12 12:18:53 +0530

    Initial Commit

---
+++ voitodo/voitodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  voitodoApp.swift
+//  voitodo
+//
+//  Created by Parasuram Krishnan on 12/03/26.
+//
+
+import SwiftUI
+
+@main
+struct voitodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
